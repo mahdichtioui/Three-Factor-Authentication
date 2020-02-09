9486b758069e9c8b31c5b4b1e3e754fc2664343a
Mahdi Chtioui <mahdichtioui19@gmail.com>
2020-02-09 14:36:57 +0100

    Sign Out Commit

--- app/src/main/java/com/mpdam/threefactorsauthentication/HomeActivity.java
+++ app/src/main/java/com/mpdam/threefactorsauthentication/HomeActivity.java
@@ -1,13 +1,21 @@
 package com.mpdam.threefactorsauthentication;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Button btnSignOut;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +23,23 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         whiteNotificationBar();
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btnSignOut = findViewById(R.id.btn_sign_out);
+
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signOut();
+            }
+        });
+    }
+
+    private void signOut(){
+        mAuth.signOut();
+        startActivity(new Intent(HomeActivity.this, MainActivity.class));
+        finishAffinity();
     }
 
     private void whiteNotificationBar() {
